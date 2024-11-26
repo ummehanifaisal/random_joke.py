ff376ebde7c1f7d071e894c9041e8f94927669e3
ummehanifaisal <ummehanifaisal86@gmail.com>
2024-11-26 01:09:23 -0800

    done

---
+++ random_joke_generator.py
@@ -0,0 +1,12 @@
+import random
+
+# Random Joke Generator
+jokes = [
+    "Why don't scientists trust atoms? Because they make up everything!",
+    "Why did the scarecrow win an award? Because he was outstanding in his field!",
+    "Why don't skeletons fight each other? They don't have the guts.",
+    "What do you call fake spaghetti? An impasta!"
+]
+
+print("Here's a random joke for you:")
+print(random.choice(jokes))
